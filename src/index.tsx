86710bc56a3fb93bad152e5f05c23de8d19c4d67
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-01-22 13:49:26 +0100

    Rename store imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import boardStore from './stores/boardStore';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <Provider store={boardStore}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
