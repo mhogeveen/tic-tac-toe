a5f126ea68cb0063981a567a4a6fc0f24d835bf0
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-01-24 18:29:31 +0100

    Create resetboard component

---
+++ src/components/ResetBoard.tsx
@@ -0,0 +1,29 @@
+import './ResetBoard.scss';
+
+import { resetBoard } from '../redux/boardSlice'
+import { resetPlayer } from '../redux/playerSlice'
+import { useAppDispatch } from '../redux/hooks'
+
+import { MdOutlineReplay } from 'react-icons/md'
+
+interface ResetBoardProps {
+  show: boolean
+}
+
+const ResetBoard = ({ show }: ResetBoardProps) => {
+  const dispatch = useAppDispatch()
+
+  const handleResetClick = () => {
+    dispatch(resetBoard())
+    dispatch(resetPlayer())
+  }
+
+  return (
+    <button className={show ? "reset-board" : "reset-board reset-board--deactivate"} onClick={handleResetClick}>
+      Reset game
+      <MdOutlineReplay />
+    </button>
+  )
+}
+
+export default ResetBoard
