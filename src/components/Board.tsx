a79e3df517acc131ff14013aaae167f7a29e34c5
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-01-24 18:28:01 +0100

    Map through tiles to create board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,11 @@
 import './Board.scss'
+import { useAppSelector } from '../redux/hooks'
 import { MdClose, MdRadioButtonUnchecked } from 'react-icons/md'
+import BoardTile from './BoardTile'
+const Board = () => {
+  const boardTiles = useAppSelector(state => state.board)
+  return (
+    <section className="board">
         <div className="board__up-next">
           <span className={`board__icon ${player === 'X' && !victoryState.gameOver ? 'board__icon--current' : ''} ${victoryState.victoriousPlayer === 'X' ? 'board__icon--winner' : ''}`}>
             <MdClose/>
@@ -8,3 +14,19 @@ import { MdClose, MdRadioButtonUnchecked } from 'react-icons/md'
             <MdRadioButtonUnchecked/>
           </span>
         </div>
+        <div className={victoryState.gameOver ? "board__grid board__grid--deactivated" : "board__grid"}>
+          {boardTiles.map(({ index, clicked, clickedBy }) => (
+            <BoardTile
+              key={index}
+              index={index}
+              clicked={clicked}
+              clickedBy={clickedBy}
+              player={player}
+            />
+          ))}
+        </div>
+    </section>
+  )
+}
+
+export default Board
