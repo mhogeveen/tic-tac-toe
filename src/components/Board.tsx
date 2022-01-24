46ebb3506ce4cb794d5ac86e66edd10ee48a52d0
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-01-24 18:26:55 +0100

    Import board styles

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,3 +1,4 @@
+import './Board.scss'
 import { MdClose, MdRadioButtonUnchecked } from 'react-icons/md'
         <div className="board__up-next">
           <span className={`board__icon ${player === 'X' && !victoryState.gameOver ? 'board__icon--current' : ''} ${victoryState.victoriousPlayer === 'X' ? 'board__icon--winner' : ''}`}>
